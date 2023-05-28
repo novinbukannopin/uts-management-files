523dfb4ccefc9fc543628a31b17af1f551d47a98
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2023-05-28 20:56:38 +0700

    fix filter is logged in with auth

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -9,9 +9,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         $data = [
-            'nama_user' => "angel",
-            "email_user" => "angel@dev.com",
-            "password_user" => password_hash('angel', PASSWORD_BCRYPT)
+            'nama_user' => "novinn",
+            "email_user" => "novinn@dev.com",
+            "password_user" => password_hash('novinn', PASSWORD_BCRYPT)
         ];
         // kalau array array, pakai insert batch
         $this->db->table('users')->insert($data);
